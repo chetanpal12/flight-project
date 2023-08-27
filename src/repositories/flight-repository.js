1183fb67786094cc3d963ff1c7948488816b68bf
chetan pal <cpal22387@gmail.com>
2023-08-27 11:52:59 +0530

    Added complex JOIN QUERY for fetchin details of all airports

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository=require('./crud-repository')
-const {Flight}=require('../models')
+const {Flight,Airplane,Airport}=require('../models')
+const {Sequelize,Op}=require('sequelize');
 class FlightRepository extends CrudRepository{
     constructor(){
         super(Flight);
@@ -7,7 +8,37 @@ class FlightRepository extends CrudRepository{
     async getAllFlights(filter,sort){
         const responce=await Flight.findAll({
             where:filter,
-            order:sort
+            order:sort,
+            // include:{
+            //     model:Airplane,            //If we write this then it will outer join for innner join you have to do:
+            //     required:true                        //  required:true,
+            // }
+            //we can include in the arrat as well
+            include:[
+                {
+                    model:Airplane,
+                    required:true,
+                    as:'airplane_detail'
+                },
+                {
+                    model:Airport,            //if we write only this 2 lines then it will not perform the exact query we want
+                    required:true,
+                    as: 'departure_airport',
+                    on:{
+                        col:Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=" , Sequelize.col("departure_airport.code"))
+//if you want to aliash then watch vide0 66 implementing seats and starting new services 55:00     
+                    },
+
+                },
+                {
+                    model:Airport,            //if we write only this 2 lines then it will not perform the exact query we want
+                    required:true,
+                    as: 'arrival_airport',
+                    on:{
+                        col:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=" , Sequelize.col("arrival_airport.code"))
+                    },
+                }
+            ]
         });
         
         return responce;
