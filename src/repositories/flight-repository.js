dc60abd1f878da64278066a2406ac7259b89b774
chetan pal <cpal22387@gmail.com>
2023-09-17 23:25:39 +0530

    Adding transaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -43,15 +43,23 @@ class FlightRepository extends CrudRepository{
     }
     
     async updateRemainingSeats(flightsId,seats,dec=true){
-        await db.sequelize.query(addRowLockOnFlights(flightsId)); //ADDING ROW LEVEL LOCK 
-        const flight=await Flight.findByPk(flightsId);
-        if(+dec){
-            await flight.decrement('totalSeats',{by:seats});
-            
-        }else{
-            await flight.increment('totalSeats',{by:seats});
+        const transaction=await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightsId)); //ADDING ROW LEVEL LOCK 
+            const flight=await Flight.findByPk(flightsId);
+            if(+dec){
+                await flight.decrement('totalSeats',{by:seats},{transaction:transaction});
+                
+            }else{
+                await flight.increment('totalSeats',{by:seats},{transaction:transaction});
+            }
+            await transaction.commit();
+            return flight;
+        } catch (error) {
+            await transaction.rollback()
+            throw error
         }
-        return flight;
+        
     }
 }   
 
